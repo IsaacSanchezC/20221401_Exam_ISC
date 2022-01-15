2159628c57c3a45902f71202f537d08aa4f21494
IsaacSanchezC <97764496+IsaacSanchezC@users.noreply.github.com>
2022-01-15 03:35:10 -0600

    Uploading Products Service
    Service REST API with net core - Repository Patern

---
+++ ProductService/ChubbyProducts/Data/EFCore/CoreRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace ChubbyProducts.Data.EFCore
+{
+    public abstract class CoreRepository<TEntity, TContext> : IRepository<TEntity>
+        where TEntity : class, IEntity
+        where TContext : DbContext
+    {
+        private readonly TContext context;
+        public CoreRepository(TContext context)
+        {
+            this.context = context;
+        }
+        public async Task<TEntity> Add(TEntity entity)
+        {
+            context.Set<TEntity>().Add(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<TEntity> Delete(int id)
+        {
+            var entity = await context.Set<TEntity>().FindAsync(id);
+            if (entity == null)
+            {
+                return entity;
+            }
+
+            context.Set<TEntity>().Remove(entity);
+            await context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<TEntity> Get(int id)
+        {
+            return await context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<List<TEntity>> GetAll()
+        {
+            return await context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<List<TEntity>> GetbyName(string name)
+        {
+            return await context.Set<TEntity>().Where(x=> x.Name.Contains(name)).ToListAsync();
+        }
+
+        public async Task<TEntity> Update(TEntity entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+    }
+}
